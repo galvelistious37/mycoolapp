11ce39422fa85f286d628c76fd2e81f549ee5fe0
johnny <john.robertson88@yahoo.com>
2024-03-24 22:05:36 -0700

    Initial commit with HelloWorld Spring app

---
+++ src/main/java/com/johnny/spring/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.johnny.spring.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // Expose a "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";git
+    }
+}
---
+++ src/test/java/com/johnny/spring/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.spring.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
